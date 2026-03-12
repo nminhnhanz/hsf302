5772bf41a790f941cb80848b73a52b5560737845
Nguyen Minh Nhan <161313041+nminhnhanz@users.noreply.github.com>
2026-03-12 21:38:00 +0700

    Delete HSFNEWS directory

--- HSFNEWS/src/main/java/com/fpt/sb/hsfnews/entity/ArticleStatus.java
+++
@@ -1,8 +0,0 @@
-package com.fpt.sb.hsfnews.entity;
-
-public enum ArticleStatus {
-
-    DRAFT,
-    PUBLISHED
-
-}
--- HSFNEWS/src/main/java/com/fpt/sb/hsfnews/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.fpt.sb.hsfnews.entity;
-
-public enum Role {
-    ADMIN,
-    EDITOR
-}
